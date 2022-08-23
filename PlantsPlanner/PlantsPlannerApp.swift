4644ed3c7a3dd0ceb1930e4d637d9ce5d5bf90dc
Volha Ivanova <keetmine@gmail.com>
2022-08-23 20:23:51 +0300

    Initial Commit

---
+++ PlantsPlanner/PlantsPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantsPlannerApp.swift
+//  PlantsPlanner
+//
+//  Created by Volha Ivanova on 23.08.22.
+//
+
+import SwiftUI
+
+@main
+struct PlantsPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
