31b7a053d848ccc68ce720a6ea27b7bdba2b5cdb
Volha Ivanova <keetmine@gmail.com>
2022-11-29 14:50:23 +0200

    badges and app icon

---
+++ PlantsPlanner/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  PlantsPlanner
+//
+//  Created by Volha Ivanova on 29.11.22.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
